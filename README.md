4aa13703f0beaae6aea3fb6c995f3e6f7e561514
Neil O'Toole <neilotoole@apache.org>
2020-03-06 23:37:29 -0700

    laying down first code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sqlitr
-sqlitr is a trivial SQLite query tool to demonstrate the capabilities of neilotoole/xcgo
+# xcgo-sqlitr
+Trivial sqlite3 query app that demonstrates neilotoole/xcgo
+
+
+The `example/sakila_tiny.sqlite` SQLite database is a tiny
+stripped-down version of the [Sakila DB](https://dev.mysql.com/doc/sakila/en/)
+with just a few rows in only one table (`actor`).
---
+++ main.go
@@ -0,0 +1,175 @@
+// Package main implements the sqlitr demo CLI for neilotoole/xcgo.
+// The program executes a query against a SQLite database.
+package main
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"reflect"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	ctx, cancelFn := context.WithCancel(context.Background())
+	defer cancelFn()
+
+	go func() {
+		stop := make(chan os.Signal, 1)
+		signal.Notify(stop, os.Interrupt)
+
+		<-stop
+		cancelFn()
+	}()
+
+	if len(os.Args) == 2 {
+		switch os.Args[1] {
+		case "help", "-h", "-help", "--help":
+			fmt.Printf(msgHelp)
+			return
+		}
+	}
+
+	err := execute(ctx, os.Stdout, os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func execute(ctx context.Context, out io.Writer, args []string) error {
+	// args[0] is sqlite db file path
+	// args[1] is the SQL query
+	// any additional args are arguments to the SQL query
+	if len(args) < 2 {
+		return errors.New("invalid args")
+	}
+
+	// Create a []interface{} containing any query args
+	var queryArgs []interface{}
+	// If there's additional args, we append them to queryArgs
+	for i := 2; i < len(args); i++ {
+		queryArgs = append(queryArgs, args[i])
+	}
+
+	// args[0] is the filename
+	db, err := sql.Open("sqlite3", args[0])
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	query := strings.TrimSpace(args[1])
+	// If it's a SELECT, we use db.QueryContext; otherwise db.ExecContext
+	if strings.HasPrefix(strings.ToUpper(query), "SELECT") {
+		// It's SELECT
+		rows, err := db.QueryContext(ctx, query, queryArgs...)
+		if err != nil {
+			return err
+		}
+
+		colNames, err := rows.Columns()
+		if err != nil {
+			return err
+		}
+
+		dests := make([]interface{}, len(colNames))
+		for i := range dests {
+			// Just make everything into a string, SQLite will handle it fine
+			dests[i] = &sql.NullString{}
+		}
+
+		w := csv.NewWriter(out)
+		w.Comma = '\t'
+		var headerWritten bool
+
+		record := make([]string, len(dests))
+		for rows.Next() {
+			err = rows.Scan(dests...)
+			if err != nil {
+				return err
+			}
+
+			for i := range dests {
+				switch v := dests[i].(type) {
+				case nil:
+					record[i] = ""
+				case driver.Valuer:
+					val, err := v.Value()
+					if err != nil {
+						return err
+					}
+					record[i] = fmt.Sprintf("%v", val)
+				default:
+					val := reflect.ValueOf(v).Elem()
+					record[i] = fmt.Sprintf("%v", val)
+				}
+			}
+
+			if !headerWritten {
+				err := w.Write(colNames)
+				if err != nil {
+					return err
+				}
+				headerWritten = true
+			}
+
+			w.Write(record)
+			w.Flush()
+		}
+
+		// Done with SELECT query
+		return nil
+	}
+
+	// Else it's not a SELECT, it's some other SQL statement
+	res, err := db.ExecContext(ctx, query, queryArgs...)
+	if err != nil {
+		return err
+	}
+	affected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Rows Affected: %d\n", affected)
+
+	lastInserted, err := res.LastInsertId()
+	if err == nil && lastInserted > 0 {
+		// We don't care about reporting the err from LastInsertId
+		fmt.Printf("Last Insert ID: %d\n", lastInserted)
+	}
+
+	return nil
+}
+
+const (
+	msgUsage = `Usage: sqlitr path/to.db query [args]
+
+Examples:
+  sqlitr --help
+  sqlitr ./example.sqlite 'SELECT * FROM actor'
+  sqlitr ./example.sqlite "INSERT INTO actor (actor_id, first_name, last_name) VALUES(11, 'Kubla', 'Khan')"
+  sqlitr ./example.sqlite 'DELETE FROM actor WHERE first_name = ?' Genghis
+`
+	msgHelp = `sqlitr is a trivial query tool for SQLite.
+
+` + msgUsage + `
+Note that if the query starts with SELECT, output is in TSV (tab-separated)
+format. If it's some other SQL statement, the count of rows affected (and
+the last insert ID if applicable) are printed.
+
+sqlitr exists solely as a demonstration for neilotoole/xcgo which
+is a Go cross-compiling docker builder image. sqlitr was created
+by Neil O'Toole <neilotoole@apache.org> and is released under
+the MIT License. It is entirely unsupported and will not be developed
+further. See https://github.com/neilotoole/sqlitr for more.
+`
+)
