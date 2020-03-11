164a91b1705d6d337c87c704c1ba276e9c80ddca
Neil O'Toole <neilotoole@apache.org>
2020-03-11 04:39:53 -0600

    working on goreleaser, again

--- goreleaser-release.sh
+++ goreleaser-release.sh
@@ -3,5 +3,6 @@
 docker run --rm --privileged \
 -v $(pwd):/go/src/github.com/neilotoole/sqlitr \
 -v /var/run/docker.sock:/var/run/docker.sock \
+-e "GITHUB_TOKEN=$GITHUB_TOKEN" \
 -w /go/src/github.com/neilotoole/sqlitr \
 neilotoole/xcgo:latest goreleaser --rm-dist
