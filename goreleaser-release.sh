ddf1f3aed994f005f0011f3c19fed724862a7fc1
Neil O'Toole <neilotoole@apache.org>
2020-03-11 04:34:21 -0600

    working on goreleaser

--- goreleaser-release.sh
+++ goreleaser-release.sh
@@ -4,4 +4,4 @@ docker run --rm --privileged \
 -v $(pwd):/go/src/github.com/neilotoole/sqlitr \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -w /go/src/github.com/neilotoole/sqlitr \
-neilotoole/xcgo:latest goreleaser --snapshot --rm-dist
+neilotoole/xcgo:latest goreleaser --rm-dist
