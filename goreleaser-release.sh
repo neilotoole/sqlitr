7138dfcdf96e63bae0e7bdc7a16f8c2f4ac5728e
Neil O'Toole <neilotoole@apache.org>
2020-03-13 20:01:16 -0600

    fiddling with goreleaser

--- goreleaser-release.sh
+++ goreleaser-release.sh
@@ -4,6 +4,7 @@ docker run --rm --privileged \
 -v $(pwd):/go/src/github.com/neilotoole/sqlitr \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -e "GITHUB_TOKEN=$GITHUB_TOKEN" \
+-e "SNAPCRAFT_LOGIN_FILE=/.snapcraft.login" \
 -v "${HOME}/.snapcraft.login":/.snapcraft.login \
 -w /go/src/github.com/neilotoole/sqlitr \
 neilotoole/xcgo:latest goreleaser --rm-dist
